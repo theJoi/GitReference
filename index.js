1c6220edf6d340600602323adb68dd4ede8bb107
git-joi <joiashlei@gmail.com>
2016-08-30 23:07:09 -0500

    Removed reference from command list.

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ app.controller("GitRefController", function($scope,$http) {
 
     console.log("git ref controller connected.");
     $scope.commands = commandData;
-
-    $scope.title = "";
-    $scope.longDescription = "Select a git command from the list to the left to see a full description with examples.";
+    $scope.pickedCommand = {};
+    $scope.pickedCommand.title = "";
+    $scope.pickedCommand.longDescription = "Select a git command from the list to the left to see a full description with examples.";
     $scope.activeCommand = "";
 
     $scope.displayDesc = function(command){
-        console.log(command);
-        console.log($scope.commands[command]['description']);
-        $scope.title = command;
-        $scope.longDescription = $scope.commands[command]['description'];
-        console.log($scope.longDescription);
+        $scope.pickedCommand.title = command;
+        $scope.pickedCommand.longDescription = $scope.commands[command]['description'];
         $scope.activeCommand = command;
+        $scope.pickedCommand.reference = "For more info about this command: ";
+        $scope.pickedCommand.link =  $scope.commands[command]['reference'];
     };
 });
