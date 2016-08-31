145506c9885f9eff77ca02be1740bd854b0bf919
git-joi <joiashlei@gmail.com>
2016-08-30 22:39:22 -0500

    Basic design completed.

---
+++ index.js
@@ -0,0 +1,21 @@
+/*jshint undef: false, browser: true*/
+var app = angular.module("gitref", []);
+
+app.controller("GitRefController", function($scope,$http) {
+
+    console.log("git ref controller connected.");
+    $scope.commands = commandData;
+
+    $scope.title = "";
+    $scope.longDescription = "Select a git command to see a full description with examples.";
+    $scope.activeCommand = "";
+
+    $scope.displayDesc = function(command){
+        console.log(command);
+        console.log($scope.commands[command]['description']);
+        $scope.title = command;
+        $scope.longDescription = $scope.commands[command]['description'];
+        console.log($scope.longDescription);
+        $scope.activeCommand = command;
+    };
+});
---
+++ reference.js
@@ -0,0 +1,29 @@
+var commandData = {
+  "config" : {
+    "name" : "config",
+    "type" : "Setup and Configure",
+    "description" : "Get and set repository or global options",
+    "flags" : "--list (list all variables set)",
+    "reference" : "https://git-scm.com/docs/git-config"
+  },
+  "help" : {
+    "name" : "help",
+    "type" : "Setup and Configure",
+    "description" : "Display help information about Git; same as git --help",
+    "flags" : "--all or -a (list all commands)\n--guide or -g (list useful guides)",
+    "reference" : "https://git-scm.com/docs/git-help"
+  },
+  "init" : {
+    "name" : "init",
+    "type" : "Getting and Creating Projects",
+    "description" : "Create an empty Git repository or reinitialize an existing one",
+    "flags" : "--bare create a bare repo",
+    "reference" : "https://git-scm.com/docs/git-init"
+  },
+  "clone" : {
+    "name" : "clone",
+    "type" : "Getting and Creating Projects",
+    "description" : "Clone a repository into a new directory",
+    "flags" : "--local, -l",
+    "reference" : "https://git-scm.com/docs/git-clone"
+  }};
