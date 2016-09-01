f4c873062cf7dae0a5571f14b33f24d6f29c596b
theJoi <joiashlei@gmail.com>
2016-08-31 22:13:45 -0500

    Added examples to reference.

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ app.controller("GitRefController", function($scope,$http,$sce) {
     $scope.commands = commandData;
     $scope.pickedCommand = {};
     $scope.pickedCommand.title = "";
-    $scope.pickedCommand.longDescription = "Select a git command from the list to the left to see a full description with examples.";
+    $scope.pickedCommand.long = "Select a git command from the list to the left to see a full description with examples.";
     $scope.activeCommand = "";
 
     $scope.displayDesc = function(command){
-        $scope.pickedCommand.title = command;
-        $scope.pickedCommand.longDescription = $sce.trustAsHtml($scope.commands[command]['description']);
         $scope.activeCommand = command;
+        $scope.pickedCommand.title = command;
+        $scope.pickedCommand.long = $sce.trustAsHtml($scope.commands[command].long);
+        $scope.pickedCommand.examples = $scope.commands[command].examples;
         $scope.pickedCommand.reference = "For more info about this command: ";
-        $scope.pickedCommand.link =  $scope.commands[command]['reference'];
+        $scope.pickedCommand.link =  $scope.commands[command].reference;
     };
 });
--- reference.js
+++ reference.js
@@ -1,29 +1,216 @@
 var commandData = {
-  "config" : {
-    "name" : "config",
-    "type" : "Setup and Configure",
-    "description" : "Get and set repository or global options",
-    "flags" : "--list (list all variables set)",
-    "reference" : "https://git-scm.com/docs/git-config"
-  },
-  "help" : {
-    "name" : "help",
-    "type" : "Setup and Configure",
-    "description" : "Display help information about Git; same as git --help",
-    "flags" : "--all or -a (list all commands)\n--guide or -g (list useful guides)",
-    "reference" : "https://git-scm.com/docs/git-help"
-  },
-  "init" : {
-    "name" : "init",
-    "type" : "Getting and Creating Projects",
-    "description" : "Create an empty Git repository or reinitialize an existing one",
-    "flags" : "--bare create a bare repo",
-    "reference" : "https://git-scm.com/docs/git-init"
-  },
-  "clone" : {
-    "name" : "clone",
-    "type" : "Getting and Creating Projects",
-    "description" : "Clone a repository into a new directory",
-    "flags" : "--local, -l",
-    "reference" : "https://git-scm.com/docs/git-clone"
-  }};
+    "config": {
+        "name": "config",
+        "type": "Setup & Config",
+        "fav": false,
+        "short": "Get and set repository or global options",
+        "flags": "--list (list all variables set)",
+        "long": "",
+        "reference": "https://git-scm.com/docs/git-config",
+        "keywords": [
+      "config",
+      "configuration",
+      "setup"
+    ]
+    },
+    "help": {
+        "name": "help",
+        "type": "Setup & Config",
+        "fav": true,
+        "short": "Display help information about Git (same as git --help)",
+        "flags": "--all or -a (list all commands)\n--guide or -g (list useful guides)\n",
+        "long": "This is where you go for quick help. It functions exactly the same as git --help. Additionally, you can change the behavior and default format of help using the config command (see reference).",
+        "reference": "https://git-scm.com/docs/git-help",
+        "keywords": "help",
+        "examples": [
+            {
+                "title": "no options",
+                "flags": "",
+                "desc": "list common Git commands",
+                "code": "git help"
+
+    }, {
+                "title": "ALL",
+                "flags": [
+        "--all",
+        "-a"
+      ],
+                "desc": "lists all available commands",
+                "code": "git help --all"
+
+    }, {
+                "title": "GUIDE",
+                "flags": [
+        "--guide",
+        "-g"
+      ],
+                "desc": "list useful Git guide",
+                "code": "git help --guide"
+
+    }, {
+                "title": "<command_name>",
+                "flags": "<command_name>",
+                "desc": "list info for a particular command",
+                "code": "git help clone"
+
+    }, {
+                "title": "INFO",
+                "flags": [
+        "--info",
+        "-i"
+      ],
+                "desc": "use option to display in info format",
+                "code": "git help -i clone"
+
+    }, {
+                "title": "MAN",
+                "flags": [
+        "--man",
+        "-m"
+      ],
+                "desc": "display in manual format; produces the same result as man <command-name>. for example man git-clone",
+                "code": "git help --man clone"
+
+    }, {
+                "title": "WEB",
+                "flags": [
+        "--web",
+        "-w"
+      ],
+                "desc": "displays manual page for command in HTML format",
+                "code": "git help --web clone"
+
+    }]
+    },
+    "init": {
+        "name": "init",
+        "type": "Project Admin",
+        "fav": true,
+        "short": "Create an empty Git repository or reinitialize an existing one",
+        "flags": "--bare create a bare repo",
+        "long": "<code>git init</code> is how you start using get. This command creates an empty Git repository - basically a .git directory with subdirectories for objects, refs/heads, refs/tags, and template files.  An initial HEAD file that references the HEAD of the master branch is also created. Often after initializing your repo, you will often add all your files (git code .) so they can be tracked, then commit them (git commit).",
+        "reference": "https://git-scm.com/docs/git-init",
+        "keywords": [
+      "init",
+      "initialize",
+      "create",
+      "repo",
+      "bare"
+    ]
+    },
+    "clone": {
+        "name": "clone",
+        "type": "Project Admin",
+        "fav": true,
+        "short": "Clone a repository into a new directory",
+        "flags": [
+      "--local",
+      "-l"
+    ],
+        "long": "Instead of creating an empty repo (git init), use <code>git clone [repo_url]</code> to clone an existing repo. Cloning gives you full copy of all data, including every version of every file for the history of the project. Check out the reference at the end for more info on repo urls.<br>The clone command has a lot more flags that are not listed in the examples, that are not used very often. See reference below for more info.",
+        "reference": "https://git-scm.com/docs/git-clone",
+        "keywords": [
+      "clone",
+      "copy",
+      "bare",
+      "url"
+    ],
+        "examples": [{
+                "title": "Clone a Repo",
+                "flags": "",
+                "desc": "Basic use of clone command: <code>git clone [repo-url] </code>. If you don't want your repo/directory named the same you can specify it: <code>git clone [repo-url] [repo-name]</code>",
+                "code": "git clone https://github.com/theJoi/joisCoolProject myCoolProject"
+    },
+            {
+                "title": "BARE",
+                "flags": "--bare",
+                "desc": "Make your cloned repo a bare repo. Check out this article if you're not sure what a bare repo is: <a href=\"http://www.saintsjd.com/2011/01/what-is-a-bare-git-repository/\">What is a bare git repository?</a>",
+                "code": "git clone --bare my_project my_project.git"
+    },
+            {
+                "title": "QUIET",
+                "flags": [
+        "--quiet",
+        "-q"
+      ],
+                "desc": "Do your cloning quietly (progess not reported)",
+                "code": "git clone -q joi@joiwilliams.com/gits/shhhh"
+    },
+            {
+                "title": "VERBOSE",
+                "flags": [
+        "--verbose",
+        "-v"
+      ],
+                "desc": "Do your cloning loudly (verbose progress reported)",
+                "code": "git clone -v file:///C:/gits/I-like-my-reports-verbose"
+    },
+            {
+                "title": "NO CHECKOUT",
+                "flags": [
+        "--no-checkout",
+        "-n"
+      ],
+                "desc": "Refrain from automatically checking out HEAD after cloning.",
+                "code": "git clone -n file:///C:/gits/I-wanna-do-it-myself"
+    },
+            {
+                "title": "ORIGIN",
+                "flags": [
+        "--origin [name]",
+        "-o [name]"
+      ],
+                "desc": "Instead of using the remote name <code>origin</code> call it whatever you want.",
+                "code": "git clone --origin not-the-origin"
+    },
+            {
+                "title": "BRANCH",
+                "flags": [
+        "--branch [name]",
+        "-b [name]"
+      ],
+                "desc": [
+        "Instead of having the HEAD point to the same branch as the cloned repo",
+        "pick your branch. If that branch doesn't exist it will create it."
+      ],
+                "code": "git clone -b git://Public/publicRepo myRepo -b myBranch"
+    }]
+    },
+    "git": {
+        "name": "git",
+        "type": "Git",
+        "fav": true,
+        "short": "All Hail Git!",
+        "flags": [
+      "--help",
+      "--version"
+    ],
+        "long": "Obviously you need git to use git. Precede all commands with the command <code>git</code>. In addition, use this command to get info about git and set some configurations. Here's a couple examples, for more see the reference.",
+        "reference": "https://git-scm.com/docs/git",
+        "keywords": "",
+        "elements":[
+
+            {
+                "title": "VERSION",
+                "flags": "--version",
+                "desc": "Print the version of git you're using.",
+                "code": "git --version"
+    },
+            {
+                "title": "HELP",
+                "flags": "--help",
+                "desc": "Get help. Works exactly like <code>git help</code>",
+                "code": [
+        "git --help",
+        "git --help --all"
+      ]
+    },
+            {
+                "title": "BARE",
+                "flags": "--bare",
+                "desc": "Treat this repo as a bare repo.",
+                "code": "git --bare"
+    }
+  ]
+    }
+};
